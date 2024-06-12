5f02d5c38e9419c18fc2c027db87538a21cdfbc7
AngeloAad <angelo.aad1@gmail.com>
2024-06-13 00:56:42 +0300

    formatted the file

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,34 +1,48 @@
-import {products} from '../constants/index';
-import {PopularProductsCard} from '../components';
+import { products } from "../constants/index";
+import { PopularProductsCard } from "../components";
 
 const PopularProducts = () => {
   return (
-    <section id="products" className="max-container
-    max-sm:mt-12">
-
-      <div className="flex flex-col justify-start gap-5
-      ">
-        <h2 className="text-4xl font-palanquin
-        font-bold">Our 
-        <span className="text-coral-red"> Popular </span> 
-        Products</h2>
-        <p className="lg:max-w-lg
-        mt-2 font-montserrat text-slate-gray">Enjoy the best quality and style with our popular choices. Discover comfort, great design, and value.</p>
+    <section
+      id="products"
+      className="max-container
+    max-sm:mt-12"
+    >
+      <div
+        className="flex flex-col justify-start gap-5
+      "
+      >
+        <h2
+          className="text-4xl font-palanquin
+        font-bold"
+        >
+          Our
+          <span className="text-coral-red"> Popular </span>
+          Products
+        </h2>
+        <p
+          className="lg:max-w-lg
+        mt-2 font-montserrat text-slate-gray"
+        >
+          Enjoy the best quality and style with our popular choices. Discover
+          comfort, great design, and value.
+        </p>
       </div>
 
-      <div className="mt-16 grid
+      <div
+        className="mt-16 grid
       lg:grid-cols-4 md:grid-cols-3
       sm:grid-cols-2 grid-cols-1
-      sm:gap-6 gap-14">
+      sm:gap-6 gap-14"
+      >
         {products.map((product) => (
           <div key={product.name}>
-          <PopularProductsCard 
-          {...product} />
+            <PopularProductsCard {...product} />
           </div>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default PopularProducts;
